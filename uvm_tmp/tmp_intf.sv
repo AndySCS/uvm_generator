44818cac2a8e54a85dadf9d6d5e11b97d4e58ee8
AndySCS <cssunaa@connect.ust.hk>
2026-01-27 16:41:30 +0800

    renamed interface template

--- uvm_tmp/tmp_intf.sv
+++
@@ -1,4 +0,0 @@
-interface {tmp}_intf(
-);
-
-endinterface
--- uvm_tmp/tmp_monitor.sv
+++ uvm_tmp/tmp_monitor.sv
@@ -1,24 +1,24 @@
 class {tmp}_monitor extends uvm_monitor;
 
-    virtual {tmp}_intf {tmp}_if;
-    uvm_analysis_port #({tmp}_tr) ap;
+    {f"virtual {tmp}_interface {tmp}_intf;" if interface_define else "pass"}
+    {f"uvm_analysis_port #({tmp}_tr) ap;" if uvm_analysis_port_define else "pass"} 
 
-    `uvm_component_utils({tmp}_input_monitor)
-    function new(string name = "{tmp}_input_monitor", uvm_component parent = null);
+    `uvm_component_utils({tmp}_t_monitor)
+    function new(string name = "{tmp}_monitor", uvm_component parent = null);
        super.new(name, parent);
-    endfunction //new()
+    endfunction
     
     extern function void build_phase(uvm_phase phase);
     extern virtual task main_phase(uvm_phase phase);
     
-endclass //{tmp}_input_monitor extends superClass
+endclass
 
 function void {tmp}_monitor::build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual {tmp}_intf)::get(this, "", "{tmp}_if", {tmp}_if))begin
-        `uvm_fatal("{tmp}_input_monitor", "{tmp} input_monitor fail to get {tmp} if")
-    end
-    ap = new("ap", this);
+    {f"if(!uvm_config_db#(virtual {tmp}_interface)::get(this, \"\", \"{tmp}_inft\", {tmp}_inft))begin" if interface_define else "pass"}
+    {f"    `uvm_fatal(get_name(), $sformatf(\"{tmp}_monitor fail to get {tmp}_intf\"))               " if interface_define else "pass"}
+    {f"end                                                                                           " if interface_define else "pass"} 
+    {f"ap = new(\"ap\", this);" if uvm_analysis_port_define else "pass"} 
 endfunction
 
 task {tmp}_monitor::main_phase(uvm_phase phase);
